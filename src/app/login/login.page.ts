15fa5e88230c67ca16c3662a699cf6b46c273b43
shaiknizam37 <shaiknizam37@gmail.com>
2019-03-13 17:52:05 +0530

    login http

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HTTP) { }
+  usrmsg={
+    valid:true,
+    msg:'Enter Username',
+  };
+  pwdmsg={
+    valid:true,
+    msg:'Enter Password',
+  };
   ngOnInit() {
+//https://api.github.com/users
   }
-
+  onClickSubmit(data) {
+    console.log(data);
+    this.usrmsg.valid=this.pwdmsg.valid=false;
+    this.http.get('https://api.github.com/users', {}, {})
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.log(error.status);
+        console.log(error.error); // error message as string
+        console.log(error.headers);
+      });
+  }
+  onFocus(){
+    this.usrmsg.valid=this.pwdmsg.valid=true;
+  }
 }
